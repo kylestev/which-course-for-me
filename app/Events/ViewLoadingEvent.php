52d79ed4262b408f0f79ee038ac7884cf755cc9a
Kyle Stevenson <kyle@kylestevenson.me>
2014-12-22 02:23:40 -0800

    view caching update

--- app/Events/ViewLoadingEvent.php
+++ app/Events/ViewLoadingEvent.php
@@ -2,6 +2,7 @@
 
 use Cache;
 use Input;
+use Request;
 
 class ViewLoadingEvent {
 
@@ -18,17 +19,19 @@ class ViewLoadingEvent {
 			self::$seen = true;
 		}
 
+		// dd(Request::path());
+
 		$page_num = (int) Input::get('page', 1);
 
-		$cache_key = sprintf('view:cache:%s:p%d', $view->name(), $page_num);
+		$cache_key = sprintf('view:cache:%s:p%d:path:%s', $view->name(), $page_num, Request::path());
 
-		$content = Cache::remember($cache_key, 1, function () use ($view)
+		$content = Cache::remember($cache_key, 10, function () use ($view)
 		{
 			$content = $view->__toString();
 			
 			return preg_replace('/[\n]+/', '', $content);
 		});
-		// echo();
+
 		die($content);
 	}
 
