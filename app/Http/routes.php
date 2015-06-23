f6cb4687154e922224f03ff0e96437befccfb0b8
Kyle Stevenson <kyle@kylestevenson.me>
2015-06-23 14:05:59 -0700

    upgrade to laravel 5.1

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,67 +11,55 @@ use Courses\Repositories\Subject\SubjectRepositoryInterface;
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
-*/
+ */
 
-Route::group(['namespace' => 'Api', 'domain' => sprintf('api.%s', env('APP_DOMAIN'))], function ()
-{
-	Route::get('/', [
-		'as' => 'api.root',
-		'uses' => 'ApiRootController@index'
-	]);
+Route::group(['namespace' => 'Api', 'domain' => sprintf('api.%s', env('APP_DOMAIN'))], function () {
+    Route::get('/', [
+        'as'   => 'api.root',
+        'uses' => 'ApiRootController@index',
+    ]);
 
-	Route::resource('instructors', 'InstructorController',
-					['only' => ['index', 'show']]);
+    Route::resource('instructors', 'InstructorController', ['only' => ['index', 'show']]);
 
-	Route::resource('section_types', 'SectionTypeController',
-					['only' => ['index', 'show']]);
+    Route::resource('section_types', 'SectionTypeController', ['only' => ['index', 'show']]);
 
-	Route::resource('subjects', 'SubjectController',
-					['only' => ['index', 'show']]);
+    Route::resource('subjects', 'SubjectController', ['only' => ['index', 'show']]);
 
-	Route::resource('subjects.courses', 'CourseController',
-					['only' => ['index', 'show']]);
+    Route::resource('subjects.courses', 'CourseController', ['only' => ['index', 'show']]);
 
-	Route::resource('subjects.courses.sections', 'SectionController',
-					['only' => ['index', 'show']]);
+    Route::resource('subjects.courses.sections', 'SectionController', ['only' => ['index', 'show']]);
 });
 
-Route::group(['namespace' => 'Frontend', 'domain' => env('APP_DOMAIN')], function ()
-{
-	Route::get('/', function (SubjectRepositoryInterface $repo)
-	{
-		return View::make('frontend.index')
-					->with('subjects', $repo->paginateResults(), 'data');
-	});
+Route::group(['namespace' => 'Frontend', 'domain' => env('APP_DOMAIN')], function () {
+    Route::get('/', function (SubjectRepositoryInterface $repo) {
+        return View::make('frontend.index')
+        ->with('subjects', $repo->paginateResults(), 'data');
+    });
 
-	Route::get('architecture', ['as' => 'frontend.architecture', function ()
-	{
-		return View::make('frontend.architecture')
-					->with('title', 'Which Course For Me | Architecture');
-	}]);
+    Route::get('architecture', ['as' => 'frontend.architecture', function () {
+        return View::make('frontend.architecture')
+        ->with('title', 'Which Course For Me | Architecture');
+    }]);
 
-	Route::get('subjects', [
-		'as' => 'frontend.subjects.index',
-		'uses' => 'SubjectController@index'
-	]);
+    Route::get('subjects', [
+        'as'   => 'frontend.subjects.index',
+        'uses' => 'SubjectController@index',
+    ]);
 
-	Route::get('subjects/{subject}/courses', [
-		'as' => 'frontend.subjects.show',
-		'uses' => 'SubjectController@show'
-	]);
+    Route::get('subjects/{subject}/courses', [
+        'as'   => 'frontend.subjects.show',
+        'uses' => 'SubjectController@show',
+    ]);
 
-	Route::get('subjects/{subject}/courses/{course}', [
-		'as' => 'frontend.courses.show',
-		'uses' => 'CourseController@show'
-	]);
+    Route::get('subjects/{subject}/courses/{course}', [
+        'as'   => 'frontend.courses.show',
+        'uses' => 'CourseController@show',
+    ]);
 
 });
 
-if (App::environment() == 'local')
-{
-	Route::get('scrape', function ()
-	{
-		Queue::push('Courses\Jobs\Scraper\ScrapeSubjects@scrape', ['term' => 'W15', 'campus' => 'corvallis']);
-	});
+if (App::environment() == 'local') {
+    Route::get('scrape', function () {
+        Queue::push('Courses\Jobs\Scraper\ScrapeSubjects@scrape', ['term' => 'F15', 'campus' => 'corvallis']);
+    });
 }
-
--- app/helpers.php
+++ app/helpers.php
@@ -7,72 +7,57 @@
  */
 function is_assoc($array)
 {
-	return (bool) count(array_filter(array_keys($array), 'is_string'));
+    return (bool) count(array_filter(array_keys($array), 'is_string'));
 }
 
 function choose_case($idx, $word)
 {
-	$no_caps = array('in', 'of', 'to', 'and', 'be', 'for', 'the');
-	$always_caps = array('ii', 'iii', 'iv', 'v', 'vi', 'osu', 'usa', 'u.s.', 'esl');
+    $no_caps     = array('in', 'of', 'to', 'and', 'be', 'for', 'the');
+    $always_caps = array('ii', 'iii', 'iv', 'v', 'vi', 'osu', 'usa', 'u.s.', 'esl');
 
-	if (in_array($word, $always_caps))
-	{
-		return strtoupper($word);
-	}
-	elseif (! in_array($word, $no_caps) || $idx === 0)
-	{
-		return ucfirst($word);
-	}
+    if (in_array($word, $always_caps)) {
+        return strtoupper($word);
+    } elseif (!in_array($word, $no_caps) || $idx === 0) {
+        return ucfirst($word);
+    }
 
-	return $word;
-}
-
-function title_case($s)
-{
-	$words = explode(' ', strtolower($s));
-	foreach ($words as $key => $word)
-	{
-		$words[$key] = choose_case($key, $word);
-	}
-
-	return implode(' ', $words);
+    return $word;
 }
 
 function osu_catalog($subj_id, $course_id)
 {
-	return sprintf('http://catalog.oregonstate.edu/CourseDetail.aspx?subjectcode=%s&coursenumber=%s', $subj_id, $course_id);
+    return sprintf('http://catalog.oregonstate.edu/CourseDetail.aspx?subjectcode=%s&coursenumber=%s', $subj_id, $course_id);
 }
 
 function term_name($term)
 {
-	$mapping = ['F' => 'Fall', 'W' => 'Winter', 'Sp' => 'Spring', 'Su' => 'Summer'];
+    $mapping = ['F' => 'Fall', 'W' => 'Winter', 'Sp' => 'Spring', 'Su' => 'Summer'];
 
-	$year = substr($term, -2);
-	$quarter = substr($term, 0, strlen($term) - 2);
+    $year    = substr($term, -2);
+    $quarter = substr($term, 0, strlen($term) - 2);
 
-	return sprintf('%s 20%s', $mapping[$quarter], $year);
+    return sprintf('%s 20%s', $mapping[$quarter], $year);
 }
 
 function extract_times($section)
 {
-	$times = array_get($section, 'raw_times');
-	if (str_contains($times, 'TBA'))
-	{
-		return $times;
-	}
+    $times = array_get($section, 'raw_times');
+    if (str_contains($times, 'TBA')) {
+        return $times;
+    }
 
-	$parts = explode('<br>', $times);
-	$parts = array_slice($parts, 0, sizeof($parts) - 1);
+    $parts = explode('<br>', $times);
+    $parts = array_slice($parts, 0, sizeof($parts) - 1);
 
-	return implode('<br />', $parts);
+    return implode('<br />', $parts);
 }
 
 function cdn_url($resource)
 {
-	return sprintf('//%s/%s', env('APP_CDN'), $resource);
+    return sprintf('//%s/%s', env('APP_CDN'), $resource);
 }
 
 function pagination_pages()
 {
-	return (int) env('PAGINATION_LENGTH') ?: 25;
+    return (int) env('PAGINATION_LENGTH') ?: 25;
 }
