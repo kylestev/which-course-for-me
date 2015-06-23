86ca0d1a0dfc7c2f9471313e6e5dd7283a8ce860
Kyle Stevenson <kyle@kylestevenson.me>
2015-06-23 14:06:20 -0700

    update the URLs for the root API index

--- app/Http/Controllers/Api/ApiRootController.php
+++ app/Http/Controllers/Api/ApiRootController.php
@@ -1,63 +1,60 @@
 <?php namespace Courses\Http\Controllers\Api;
 
+use Courses\Http\Controllers\Controller;
 use Illuminate\Contracts\Cache\Factory as CacheFactory;
 use Illuminate\Http\JsonResponse;
 
-use Courses\Http\Controllers\Controller;
+class ApiRootController extends Controller
+{
 
-class ApiRootController extends Controller {
+    use TraitTransformer;
 
-	use TraitTransformer;
+    protected $cache;
 
-	protected $cache;
+    public function __construct(
+        CacheFactory $cache,
+        JsonResponse $response
+    ) {
+        $this->cache       = $cache;
+        $this->response    = $response;
+        $this->transformer = null;
+    }
 
-	public function __construct(
-		CacheFactory $cache,
-		JsonResponse $response
-	)
-	{
-		$this->cache = $cache;
-		$this->response = $response;
-		$this->transformer = null;
-	}
+    private function getRoutes()
+    {
+        $content = [];
 
-	private function getRoutes()
-	{
-		$content = [];
+        $routes = collect(self::$router->getRoutes()->getRoutes())->filter(function ($route) {
+            return !str_is('frontend.*', $route->getName()) && str_is('*.show', $route->getName());
+        })->map(function ($route) {
+            $parts = explode('.', $route->getName());
+            $last_idx = sizeof($parts) - 1;
+            $name = implode('_', array_slice($parts, 0, $last_idx)) . '_url';
 
-		$routes = array_where(self::$router->getRoutes(), function ($key, $route) {
-			return (! str_is('frontend.*', $route->getName())) && str_is('*.show', $route->getName());
-		});
+            $uri = getenv('API_URL') . '/';
+            $uri .= str_replace('s}', '_id}', $route->uri());
 
-		$routes = array_map(function ($route) {
-			$parts = explode('.', $route->getName());
-			$last_idx = sizeof($parts) - 1;
-			$name = implode('_', array_slice($parts, 0, $last_idx)) . '_url';
+            return ['name' => $name, 'uri' => $uri];
+        });
 
-			$uri = getenv('APP_URL') . '/';
-			$uri .= str_replace('s}', '_id}', $route->uri());
+        foreach ($routes as $route) {
+            $content[$route['name']] = $route['uri'];
+        }
 
-			return ['name' => $name, 'uri' => $uri];
-		}, $routes);
+        return $content;
+    }
 
-		foreach ($routes as $route) {
-			$content[$route['name']] = $route['uri'];
-		}
+    private function getCachedRoutes()
+    {
+        return $this->cache->remember('api.routes', 1, function () {
+            return $this->getRoutes();
+        });
+    }
 
-		return $content;
-	}
-
-	private function getCachedRoutes()
-	{
-		return $this->cache->remember('routes.transformed', 1, function () {
-			return $this->getRoutes();
-		});
-	}
-
-	public function index()
-	{
-		$content = $this->getCachedRoutes();
-		return $this->createJsonResponse($content);;
-	}
+    public function index()
+    {
+        $content = $this->getCachedRoutes();
+        return $this->createJsonResponse($content);
+    }
 
 }
